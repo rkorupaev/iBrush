faf0efc9d77f760379246820586f6b806d9d7c31
Korupaev Roman <r.korupaev@norsi-trans.lcl>
2021-08-18 15:12:00 +0300

    mobile started

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -1,6 +1,6 @@
 import {swiper} from "./swiper";
 
-swiper('.swiper-container');
+swiper(`.swiper-container`);
 
 import Choices from "choices.js";
 
@@ -16,8 +16,6 @@ const test = () => {
       item: `choices__item form__select-item`
     }
   });
-
-  element.closest(`.choices`).querySelector(`.choices__list--dropdown`).querySelector(`.choices__list`).firstChild.style.display = `none`;
 };
 
 test();

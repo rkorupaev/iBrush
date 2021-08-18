f8dc69d5c9da837c90b8e23f37b77a0b73bc14df
Korupaev Roman <r.korupaev@norsi-trans.lcl>
2021-08-18 13:28:39 +0300

    custom select half done

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -1,3 +1,23 @@
 import {swiper} from "./swiper";
 
 // swiper('.swiper-container');
+
+import Choices from "choices.js";
+
+const test = () => {
+  const element = document.querySelector(`.form__select`);
+  const choices = new Choices(element, {
+    placeholder: false,
+    itemSelectText: ``,
+    searchEnabled: false,
+    classNames: {
+      containerOuter: `choices input--top-block form__select`,
+      containerInner: `choices__inner form__select-inner`,
+      item: `choices__item form__select-item`
+    }
+  });
+
+  element.closest(`.choices`).querySelector(`.choices__list--dropdown`).querySelector(`.choices__list`).firstChild.style.display = `none`;
+};
+
+test();
